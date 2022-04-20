e1b34cec2b40332995dff575f556cc1e2c332015
patriciabrsk <patriciabresku@gmail.com>
2022-04-20 17:35:22 +0200

    snack5

--- snack5/index.php
+++ snack5/index.php
@@ -2,7 +2,15 @@
     // Prendere un paragrafo abbastanza lungo, contenente diverse frasi. 
     // Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
     
-    
+    $paragraph = "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. 
+    He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. 
+    The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. 
+    \"What's happened to me?\" he thought. It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls. A collection of textile samples 
+    lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. 
+    It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. 
+    Gregor then turned to look out the window at the dull weather.";
+    $paragraphs_by_dot = explode(".", $paragraphs);
+    var_dump($paragraphs_by_dot);
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +22,12 @@
     <title>Snack 5</title>
 </head>
 <body>
-    <main></main>
+    <main>
+        <?php for($i = 0; $i < count($paragraphs_by_dot); $i++) {?>
+            <p>
+                <?php echo $paragraphs_by_dot[$i]; ?>
+            </p>
+        <?php }?>
+    </main>
 </body>
 </html>
